a6af4b1628876c914a62204403ed662accc7e3ad
George Tourtsinakis <tourtsinakis@outlook.com>
2016-12-31 16:20:01 +0200

    Add CSS manipulation with javascript
    - Sections for CSS manipulation
    - Adding removing elements with javascript
    - Mysterious code
    - Hidden in server file with mysterious code answers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
-/**
- * Created by George on 27-Nov-16.
- */
+/*====Pure javascript DOM manipulation with no jquery====*/
+
+
+
+//=====Manipulate CSS with javascript=====
+var products = document.querySelector('#mainSection');
+
+products.style.backgroundColor = "#f2e6d9";
+
+products.style.borderRadius = "25px";//End Manipulate CSS with javascript
+
+//Numbering mysterious code with loop
+var mysteriousCode = document.querySelectorAll(".mysteriousCode");
+
+
+for(var index in mysteriousCode){
+    mysteriousCode[index].innerHTML += " "+ index;
+}//====End Numbering mysterious code=======
+
+
+
+
+//===Adding or removing elements dynamically with javascript===========================
+var p = document.createElement("p");
+p.style.backgroundColor = "lightblue";
+p.style.padding = "5px";
+p.textContent ="Appended with javascript! How cool you are?";
+var text= document.querySelector("#text");
+
+text.appendChild(p);//===End Adding or removing elements dynamically with javascript===
+
+
+
+
+//=Mysterious Code(interview question) 14=============
+var mainArticle = document.getElementById("mainArticle");
+var newSection = document.createElement("section");
+newSection.innerHTML = "<p id='sectionParagraph'>created paragraph</p>";
+mainArticle.appendChild(newSection);
+
+var sectionParagraph = document.getElementById("sectionParagraph");
+
+sectionParagraph.style.border = "1px solid black";//==
+
